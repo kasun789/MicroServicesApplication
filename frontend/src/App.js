0f432035cc801597d52fa5d8b66986eca8b47be5
sandali kavishka <sandalikavish@gmail.com>
2023-09-24 16:47:07 +0530

    UIs for login and signup

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signUp' element={<SignUp/>}></Route>
+
+          </Routes>
+          </Router>
+    </div>
+  );
+}
+
+export default App;
