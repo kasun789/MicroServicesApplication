67c43269c5e2c979f84be4a18be9058907fbdbbd
kasun789 <kasungayashan789@gmail.com>
2023-09-25 01:27:56 +0530

    finished sign up program

---
+++ Signup/src/main/java/com/SriTel/Signup/Controller/UserController.java
@@ -0,0 +1,56 @@
+package com.SriTel.Signup.Controller;
+
+import com.SriTel.Signup.Model.User;
+import com.SriTel.Signup.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/add")
+    private String addUser(@RequestBody User user){
+        if(String.valueOf(user.getPhoneNo()).length() == 9){
+            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            userService.add(user);
+            return "User added";
+        }
+
+        return "User addition Unsuccessful";
+    }
+
+    @GetMapping("/viewAll")
+    private List<User> viewAllUser(){
+        return userService.viewAll();
+    }
+
+    @PutMapping("/update/{userId}")
+    private String updateUser(@RequestBody User user,@PathVariable int userId){
+
+        if(String.valueOf(user.getPhoneNo()).length() == 9){
+            userService.update(user,userId);
+            return "User update Successfully";
+        }
+
+        return "User update Unsuccessfully";
+
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    private String deleteUser(@PathVariable int userId){
+       return (userService.delete(userId)) ? "User Delete Successfully" : "User Delete UnSuccessfully";
+    }
+
+    @GetMapping("/viewSpecific/{userId}")
+    private User viewSpecificUser(@PathVariable int userId){
+        return userService.viewSpecificUser(userId);
+    }
+}
---
+++ Signup/src/test/java/com/SriTel/Signup/SignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SriTel.Signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
