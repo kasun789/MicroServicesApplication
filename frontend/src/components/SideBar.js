5dde921f140c433da33fe99a602cdb9f46a8518a
Thilina <thilinalakshanhewage1999@gmail.com>
2023-09-29 23:43:07 +0530

    Account balance component

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -29,30 +29,31 @@ const SideBar = () => {
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-              <span className="m-1">Home</span>
+              <span className="m-1">Dashboard</span>
             </Link>
             <Link className="flex ml-[40px] mt-[10px]" style={linkStyle}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 m-[5px]"
                 fill="none"
                 viewBox="0 0 24 24"
+                strokeWidth={1.5}
                 stroke="currentColor"
-                strokeWidth={2}
+                className="w-6 h-6 ml-[5px] mt-1 mr-[5px]"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
                 />
               </svg>
-              <span className="m-1">Inbox</span>
+
+              <span className="m-1">Profile</span>
             </Link>
 
             <Link className="flex ml-[40px] mt-[10px]" style={linkStyle}>
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 m-[5px]"
+                xmlns=""
+                className="w-8 h-8 mt-[2px] mr-[2px]"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -61,46 +62,30 @@ const SideBar = () => {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  d="M16 11V7a4 4 0 00-8 0v4M9 16h6a2 2 0 002-2V8a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zM9 12h6"
                 />
               </svg>
-              <span className="m-1">Inbox</span>
+
+              <span className="m-1">Bill & Payment</span>
             </Link>
 
             <Link className="flex ml-[40px] mt-[10px]" style={linkStyle}>
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 m-[5px]"
                 fill="none"
-                viewBox="0 0 24 24"
                 stroke="currentColor"
                 strokeWidth={2}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 m-[4px] mr-[5px]"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                />
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                ></path>
               </svg>
-              <span className="m-1">Inbox</span>
-            </Link>
 
-            <Link className="flex ml-[40px] mt-[10px]" style={linkStyle}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 m-[5px] "
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                />
-              </svg>
-              <span className="m-1">Logout</span>
+              <span className="m-1">Support</span>
             </Link>
           </div>
         </div>
